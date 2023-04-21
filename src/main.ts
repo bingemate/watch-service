c11fce0d5febc1728cd75f318b6a6c04f35f2a39
pooetitu <pooetitu@gmail.com>
2023-04-21 22:43:35 +0200

    [BM-99]: Add swagger config

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { INestApplication } from '@nestjs/common';
+
+export const swaggerConfig = (): DocumentBuilder => {
+  return new DocumentBuilder()
+    .setTitle('Watch service swagger')
+    .setVersion('v1.0');
+};
+
+export const initSwagger = (app: INestApplication) => {
+  const document = SwaggerModule.createDocument(app, swaggerConfig().build());
+  SwaggerModule.setup('openapi', app, document);
+};
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({ origin: '*' });
+
+  initSwagger(app);
   await app.listen(process.env.SERVER_PORT || 3000);
 }
 bootstrap();
