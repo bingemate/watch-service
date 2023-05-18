bc5aee6555f3ddccefce158c9416de8112707d8f
pooetitu <pooetitu@gmail.com>
2023-05-18 22:38:45 +0200

    [BM-231]: Add episode and season number for TMDB support DTO

--- src/playlist/dto/playlist-item.dto.ts
+++ src/playlist/dto/playlist-item.dto.ts
@@ -3,4 +3,8 @@ import { ApiProperty } from '@nestjs/swagger';
 export class PlaylistItemDto {
   @ApiProperty()
   mediaId: string;
+  @ApiProperty()
+  episode: number;
+  @ApiProperty()
+  season: number;
 }
