1b419aab35bf14da2ea8063c56727969a98a37cb
pooetitu <pooetitu@gmail.com>
2023-06-10 15:44:57 +0200

    [BM-336]: Fix build errors

--- src/tv-show-watch-list/dto/update-episode-watchlist-item.dto.ts
+++ src/tv-show-watch-list/dto/update-episode-watchlist-item.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { TvShowWatchListStatus } from '../episode-watch-list-status.enum';
+import { TvShowWatchListStatus } from '../tv-show-watch-list-status.enum';
 
 export class UpdateEpisodeWatchlistItemDto {
   @ApiProperty({ enum: TvShowWatchListStatus })
