432ffb64ab91f9683dfcba3594c461ad17b3a7c7
pooetitu <pooetitu@gmail.com>
2023-06-20 18:13:50 +0200

    [BM-168]: Add listener

--- src/tv-show-watch-list/tv-show-watch-list.module.ts
+++ src/tv-show-watch-list/tv-show-watch-list.module.ts
@@ -4,6 +4,7 @@ import { TvShowWatchListItemEntity } from './tv-show-watch-list-item.entity';
 import { TvShowWatchListService } from './tv-show-watch-list.service';
 import { TvShowWatchListController } from './tv-show-watch-list.controller';
 import { EpisodeWatchListItemEntity } from './episode-watch-list-item.entity';
+import { EpisodeWatchStatsListener } from './episode-watch-list.listener';
 
 @Module({
   imports: [
@@ -12,7 +13,7 @@ import { EpisodeWatchListItemEntity } from './episode-watch-list-item.entity';
       EpisodeWatchListItemEntity,
     ]),
   ],
-  providers: [TvShowWatchListService],
+  providers: [TvShowWatchListService, EpisodeWatchStatsListener],
   controllers: [TvShowWatchListController],
   exports: [TvShowWatchListService],
 })
