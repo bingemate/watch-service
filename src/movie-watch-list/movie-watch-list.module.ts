328b4931ef5c6224c7dd33be340887a289bc04bb
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2023-06-27 15:05:45 +0200

    feat: Add MovieHistoryEntity to MovieWatchListModule imports
    This commit adds the MovieHistoryEntity to the list of modules imported in the MovieWatchListModule. This change will allow for better tracking and management of movie history within the application.

--- src/movie-watch-list/movie-watch-list.module.ts
+++ src/movie-watch-list/movie-watch-list.module.ts
@@ -4,11 +4,11 @@ import { MovieWatchListItemEntity } from './movie-watch-list-item.entity';
 import { MovieWatchListService } from './movie-watch-list.service';
 import { MovieWatchListController } from './movie-watch-list.controller';
 import { MovieWatchStatsListener } from './movie-watch-list.listener';
-import {MovieHistoryEntity} from "../movie-history/movie-history.entity";
 import { MovieHistoryService } from '../movie-history/movie-history.service';
+import { MovieHistoryEntity } from '../movie-history/movie-history.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([MovieWatchListItemEntity])],
+  imports: [TypeOrmModule.forFeature([MovieWatchListItemEntity, MovieHistoryEntity])],
   providers: [MovieWatchListService, MovieWatchStatsListener, MovieHistoryService],
   controllers: [MovieWatchListController],
   exports: [MovieWatchListService],
