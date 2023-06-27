bd773e45c60f56ede58b9ab25471795dbcf692b0
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2023-06-27 14:56:02 +0200

    feat(movie-watch-list): Add MovieHistoryService to providers
    This commit adds the MovieHistoryService to the providers in the movie-watch-list module. It also updates the MovieWatchListService to use methods from the MovieHistoryService when deleting and updating watch list items. If a movie is marked as finished, it will update or create a history record for that user and movie.

--- src/movie-watch-list/movie-watch-list.service.ts
+++ src/movie-watch-list/movie-watch-list.service.ts
@@ -3,12 +3,14 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { MovieWatchListItemEntity } from './movie-watch-list-item.entity';
 import { MovieWatchListStatus } from './movie-watch-list-status.enum';
+import { MovieHistoryService } from '../movie-history/movie-history.service';
 
 @Injectable()
 export class MovieWatchListService {
   constructor(
     @InjectRepository(MovieWatchListItemEntity)
     private readonly watchListRepository: Repository<MovieWatchListItemEntity>,
+    private readonly movieHistoryService: MovieHistoryService
   ) {}
 
   async getWatchListByUserId(
@@ -36,6 +38,7 @@ export class MovieWatchListService {
 
   async deleteWatchListItem(userId: string, movieId: number) {
     await this.watchListRepository.delete({ userId, movieId: movieId });
+    await this.movieHistoryService.deleteMediaHistory(movieId, userId);
   }
 
   async updateWatchListItem(
@@ -46,5 +49,17 @@ export class MovieWatchListService {
     status: MovieWatchListStatus,
   ) {
     await this.watchListRepository.update(param, { status });
+    if (status === MovieWatchListStatus.FINISHED) {
+      const history = await this.movieHistoryService.getHistory(param.userId, param.movieId);
+      if (history) {
+        await this.movieHistoryService.updateMovieHistory(param.userId, param.movieId, 1);
+      } else {
+        await this.movieHistoryService.createMovieHistory({
+          movieId: param.movieId,
+          userId: param.userId,
+          stoppedAt: 1,
+        });
+      }
+    }
   }
 }
